4f7df82bb5f2ecccfdc87b28c97dfc5398e5703a
Rohit Dhiman <rohitdhiman937@gmail.com>
2024-02-21 12:40:58 +0530

    add eslint plugins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
