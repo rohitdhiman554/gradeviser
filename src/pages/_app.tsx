70d128f38d7b0c6bcea94b25037162c912a263cf
Rohit Dhiman <rohitdhiman937@gmail.com>
2024-02-21 12:16:42 +0530

    made queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Fragment, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+  return  <Fragment>
+  <QueryClientProvider client={queryClient}>
+  
+
+    <Component {...pageProps} />
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+  </QueryClientProvider>
+</Fragment>
 }
